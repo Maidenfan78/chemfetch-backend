b9868d23b923f3c9560c5e5df9aa27b3bedeef8c
Sav <maidenfan78@gmail.com>
2025-08-16 22:39:37 +1000

    improve sds parsing

--- ocr_service/test_issue_date_fallback.py
+++
@@ -1,7 +0,0 @@
-import parse_sds
-
-
-def test_fallback_date_from_section_16():
-    sect16 = "Date of issue / Date of\n13/04/2023 revision."
-    result = parse_sds._resolve_issue_date(sect16, "")
-    assert result == "2023-04-13"
